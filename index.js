3b67ba4e2d4d09e64fdf043909b789350e653b55
sujon-mia <sujonmia.0612@gmail.com>
2022-04-24 22:57:16 +0600

    mongo connected

--- index.js
+++ index.js
@@ -2,10 +2,27 @@ const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wf1xm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir)
 
 app.get('/', (req, res) => {
     res.send('server running');
